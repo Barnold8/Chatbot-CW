5a0cef8618a18d7f685d4c0bc116d5bc70dfe9b7
Kirsten Wright <bandslover@outlook.com>
2023-10-31 14:19:34 +0000

    Basic language model setup

--- chatbot_cw.py
+++ chatbot_cw.py
@@ -9,7 +9,6 @@ from nltk . lm . preprocessing import pad_both_ends , padded_everygram_pipeline
 # GLOBAL VARIABLE DECLARATION
 n_param = 2
 debug = False
-
 # END OF GLOBAL VARIABLE DECLARATION
 
 def tokenize(inp : str)-> list[str]:
@@ -18,6 +17,10 @@ def tokenize(inp : str)-> list[str]:
          takes input of type string, a human readable sentence and tokenizes it
     return: list of tokens of type string
     """
+
+    if debug:
+        print("DEBUG: Processing inp string...")
+
     custom_punctuation  = string.punctuation + "’" + "-" + "‘" + "-"  # Have a string containing punctuation we want to remove from our text
     custom_punctuation.replace(".", "")                               # Remove the full stop in the custom punctuation (We need this to identify what and what isnt a sentence)
 
@@ -25,17 +28,22 @@ def tokenize(inp : str)-> list[str]:
 
     "".join([char for char in inp if char not in custom_punctuation])  # Make the file contents into a singular string
 
-    if debug:
-        print("DEBUG: Processing inp string...")
-
     return [word_tokenize(sent) for sent in sent_tokenize(inp)]
 
-def padder(inp : list[str]):
-    """"""
+def padder(inp : list[str]) -> nltk.lm.models.Laplace :
+    """
+    Desc:
+        This takes in a list of tokens and then applies them to the Laplace statistical model. This helps us 
+        make a ngram (using the global n_param variable)
+    return: Laplace model
+    """
     corpus,vocab = padded_everygram_pipeline(n_param,inp)
     lm = Laplace(n_param)
     lm.fit(corpus,vocab)
     return lm
 
+lm = padder(tokenize("This is a form of a document")) # lm means language model 
 
-print(tokenize("This is a form of a document"))
+# for vocab_token in lm . vocab :
+#     score = lm . score ( vocab_token )
+#     print (" Unigram : [" + vocab_token + "] : " + str( score ))
